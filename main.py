9b47093c195280547046087a24798e8b2eb16895
BasCR-hub <63452988+BasCR-hub@users.noreply.github.com>
2020-04-10 16:50:23 +0200

    reupdate

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ for x in xrange(1, 200):
     if fizz and buzz :
         print "Forkons mon ami forkons!"
     elif fizz:
-        print "Fizz"
+        print "Nouveau truc"
     elif buzz:
         print "Buzz"
     else:
